f228d583f8934d4c1411ce376524fa213eb234ef
zaidbinusama <54680765+zaidbinusama@users.noreply.github.com>
2022-05-28 00:20:09 +0500

    Add files via upload

--- nusttickets/nusttickets/views.py
+++ nusttickets/nusttickets/views.py
@@ -17,17 +17,41 @@ from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
-from datetime import datetime
+from datetime import datetime, tzinfo, timedelta, timezone,time
+# import schedule
+# import time
+import pytz
 # Create your views here.
 
 
+def checkTicket():
+    naive = datetime.now()
+    utc = pytz.utc
+    gmt5 = pytz.timezone('Etc/GMT+5')
+    now = utc.localize(naive).astimezone(gmt5)
+    reserved = User_Event.objects.filter(purchase_type='R')
+    for ticket in reserved:
+        time_elapsed = now - ticket.date_bought
+        if time_elapsed > timedelta(minutes=2):
+            event_name = ticket.EventName
+            event = Event.objects.filter(name=event_name).first()
+            print(event)
+            event.tickets_sold -= 1
+            event.tickets_left += 1
+            event.save()
+            print(time_elapsed)
+            ticket.delete()
+
+
 def Index(request):
+    checkTicket()
     events = Event.objects.all()
     society = Organizer.objects.all()
     return render(request, 'index.html', context={'events': events, 'society': society})
 
 
 def Events(request, oid):
+    checkTicket()
     event_data = Event.objects.filter(name=oid).first()
     ticket_bought = False
     if request.user.is_authenticated:
@@ -51,12 +75,21 @@ def Events(request, oid):
          })
 
 def Tickets(request,oid):
+    checkTicket()
     event_data = Event.objects.filter(name=oid).first()
     current_user = request.user
     return render(request, 'ticketdetails.html', context={'current_user': current_user,'event_data':event_data})
 
 
+
+# def job():
+#     print("I'm working...")
+#
+# schedule.every(1).minutes.do(job)
+
+
 def SignUp(request):
+    checkTicket()
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
@@ -82,6 +115,7 @@ def SignUp(request):
 
 
 def LoginView(request):
+    checkTicket()
     if request.method == 'POST':
 
         # AuthenticationForm_can_also_be_used__
@@ -100,17 +134,20 @@ def LoginView(request):
 
 
 def LogoutView(request):
+    checkTicket()
     logout(request)
     return redirect('index')
 
 
 def Buy(request, oid):
+    checkTicket()
     event_data = Event.objects.filter(name=oid).first()
     return render(request, 'buy.html',context={'event_data':event_data})
 
 
 
 def Pay(request,oid):
+    checkTicket()
     event_data = Event.objects.filter(name=oid).first()
 
     event_data.tickets_left -= 1
@@ -124,7 +161,7 @@ def Pay(request,oid):
 
 
 def Reserve(request,oid):
-
+    checkTicket()
     event_data = Event.objects.filter(name=oid).first()
 
     event_data.tickets_left -= 1
